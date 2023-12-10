9736250319a34b9965a3c3d32071ad2fafaa4a92
mseyhan <muratseyhan7@gmail.com>
2023-12-10 11:11:24 +0300

    simplified event parser from open data

--- visualization/vizFunc.py
+++ visualization/vizFunc.py
@@ -5,6 +5,7 @@ import math
 import json
 from scipy.stats import t
 from scipy import stats
+import time
 
 # viz
 import matplotlib.pyplot as plt
@@ -41,6 +42,9 @@ palette = ["#989898","#ffc100","#ff0057","#ff0057","#1a1a1a","#0d7177"]
 with open('../config.json', 'r') as config_file:
     config = json.load(config_file)
 
+# database path
+db_path = config.get('statsbomb_db_path')
+
 def get_config_cmap(cmap_name,n=int()):
     try:
         cmap = LinearSegmentedColormap.from_list(config["cmaps"][cmap_name]["name"],
@@ -49,6 +53,13 @@ def get_config_cmap(cmap_name,n=int()):
     except:
         print("No colormap was found in config.json")
     return cmap
+
+# stored keys as json to integers for mapping
+def keystoint(x):
+    return {int(k): v for k, v in x.items()}
+
+# get formation mapping, keys stored as integers
+formation_dict = json.loads(json.dumps(config['formation_dict']['EN']), object_hook=keystoint) # set an object hook to get integers
 
 def show_color_maps(cmaps):
     fig, axes = plt.subplots(figsize=(12, 5), nrows=8, ncols=2, constrained_layout=True)
@@ -75,6 +86,9 @@ class footyviz:
         self.scattyDotColor = 'grey'
         self.title_font = {'size':'20', 'color':'black', 'weight':'bold'}
         self.font_url = 'https://github.com/google/fonts/blob/main/ofl/bebasneue/BebasNeue-Regular.ttf?raw=true'
+        self.playerNameFont = FontManager(url=config["fonts"]["rubik"])
+        self.captionFont = FontManager(url=config["fonts"]["bebas_neue"])
+        self.nameTagFont = FontManager(url=config["fonts"]["roboto_regular"])
         self.seasonNameList = [value for value in self.data.season_name.unique() if not (isinstance(value, float) and np.isnan(value))]
         self.playerName = None
         # create a colormap
@@ -82,7 +96,6 @@ class footyviz:
         self.colormap = LinearSegmentedColormap.from_list("custom_colormap", self.colors, N=100)
     def navigate(self, **filters):
         query_string = ' & '.join([f'({col} == {repr(val)})' for col, val in filters.items()])
-    
         self.data = self.data.query(query_string)
         #return self.data
     def navigate_temp(self,**filters):
@@ -101,8 +114,54 @@ class footyviz:
         plt.gca().invert_yaxis()
         # Adding a fancy title
 
-    def scatty(self):
-        sns.scatterplot(x='x', y='y', data=self.data, ec=None,alpha=1, color = self.scattyDotColor,zorder=3, legend=False)
 
+def get_selected_matches():
+    # RUN ONLY WHEN WORKING A DIFFERENT PLAYER/SEASON
+    # connect to the sqlite database
+    conn = sqlite3.connect(db_path)
 
+    with open('data/selected_matches.sql') as inserts:
+        q1 = inserts.read()
 
+    # retrieve all matchdata
+    seasonId = 27 # only for reporting, change in .sql file
+    competitionId = 2
+    query = q1
+    print(query)
+    # use the connection and query to create a dataframe
+    matches = pd.read_sql_query(query, conn)
+
+    # close the connection
+    conn.close()
+
+    # RUN ONLY WHEN WORKING A DIFFERENT PLAYER/SEASON
+    print(matches.head())
+    match_list = matches.match_id.unique().tolist()
+    return matches, match_list
+
+def parse_insert_events_stg():
+    print('Retrieve match list...')
+    match_list = get_selected_matches()[0]
+    print('match_list retrieved.')
+    # create an sqlalchemy engine to connect to the SQLite database
+    engine = create_engine(f'sqlite:///{db_path}')
+    # instantiate a parser object
+    parser = Sbopen()
+    # RUN ONLY WHEN WORKING A DIFFERENT PLAYER-SEASON
+    start_time = time.time()
+    event_df_list = []
+    print('GENERATE df_event ---------------------')
+    for i, match in enumerate(match_list):
+        print(f"{i} - ENTERED :",match)
+        event_df_list.append(parser.event(match)[0])
+        print("event appended\n---------------------")
+    print("appending complete")
+
+    df_event = pd.concat(event_df_list)
+    print("Concatenation done")
+    # in order not to do the same data retrieval over and over, using the to_sql function to write the dataframe to the sqlite database
+    df_event.to_sql('event_stg', engine, if_exists='replace', index=False)
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"It took {duration}")
+    return df_event
